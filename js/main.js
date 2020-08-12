51e212c0e0ddd1fd91b02f111ef28757f3ff4d38
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-08-12 06:36:44 -0500

    Deleted files

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdownMenu() {
-    var menu = document.getElementById('menuDiv');
-    if (menu.style.display === "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
