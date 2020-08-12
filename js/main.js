ae70a5837edee4f0d2d315dd1186d92c126381fe
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-08-12 06:05:24 -0500

    Building new jekyll site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleDropdownMenu() {
+    var menu = document.getElementById('menuDiv');
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
